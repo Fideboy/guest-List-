37902c88fb10069be02758ad6d201084dd0f2706
Confidence Utibe <confidenceutibe@gmail.com>
2023-05-31 12:25:14 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// A program to check for invited friends and uninvited friends to my 
+let guestList = ["Peter", "Pan", "Wendy", "Captain", "Hooke"];
+let guestName = prompt("What is your name?");
+if (guestList.includes(guestName)){
+         alert("Your welcome. We've beeen expecting you");
+}else {
+    alert("Sorry, maybe next time")};
